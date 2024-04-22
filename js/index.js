2f717c98a002d830cdea83ca6b2648c02b55d7b0
Alex <alexflorea123@gmail.com>
2024-04-22 12:04:28 -0400

    MapDone

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1280
-canvas.height = 768
+canvas.width = 1280;
+canvas.height = 768;
 
-c.fillStyle = 'white';
-c.fillRect(0,0, canvas.width, canvas.height);
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
 console.log(canvas);
+const image = new Image();
+image.onload = () => {
+  c.drawImage(image, 0, 0, canvas.width, canvas.height);
+};
+image.src = "img/gameMap.png";
