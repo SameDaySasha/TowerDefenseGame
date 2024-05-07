eafc106efa1bcab501b32ec6baa249d88e1f1258
Alex <alexflorea123@gmail.com>
2024-05-07 10:21:37 -0400

    WaypointsFixed2

--- js/index.js
+++ js/index.js
@@ -12,41 +12,38 @@ const image = new Image();
 image.onload = () => {};
 image.src = "img/gameMap.png";
 
-
-
 class Enemy {
-  constructor({position = { x: 0, y: 0 }}) {
-    this.position = position
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position;
     this.width = 100;
     this.height = 100;
     this.waypointIndex = 0;
   }
-  draw(){
+  draw() {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
-  update(){
+  update() {
     this.draw();
-    
-  
 
-
-    const waypoint = waypoints[this.waypointIndex]
-    const yDistance = waypoint.y - this.position.y
-    const xDistance = waypoint.x - this.position.x
+    const waypoint = waypoints[this.waypointIndex];
+    const yDistance = waypoint.y - this.position.y;
+    const xDistance = waypoint.x - this.position.x;
     const angle = Math.atan2(yDistance, xDistance);
-    
-    this.position.x += Math.cos(angle)
-    this.position.y += Math.sin(angle)
-    
-    if (Math.round(this.position.x) === Math.round(waypoint.x) && 
-       Math.round(this.position.y) === Math.round(waypoint.y)){
-      this.waypointIndex++
+
+    this.position.x += Math.cos(angle);
+    this.position.y += Math.sin(angle);
+
+    if (
+      Math.round(this.position.x) === Math.round(waypoint.x) &&
+      Math.round(this.position.y) === Math.round(waypoint.y) && this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++;
     }
   }
 }
-const enemy = new Enemy({position: { x: 200, y:400 }});
-const enemy2 = new Enemy({position: { x: 0, y:400 }});
+const enemy = new Enemy({ position: { x: 200, y: 400 } });
+const enemy2 = new Enemy({ position: { x: 0, y: 400 } });
 function animate() {
   requestAnimationFrame(animate);
 
