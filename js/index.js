1e8dda36e28efaae619681dae85fc2cc9fc0f3a2
Alex <alexflorea123@gmail.com>
2024-05-07 10:29:20 -0400

    CenteringDone

--- js/index.js
+++ js/index.js
@@ -18,6 +18,10 @@ class Enemy {
     this.width = 100;
     this.height = 100;
     this.waypointIndex = 0;
+    this.center ={
+      x:this.position.x + this.width / 2,
+      y:this.position.y + this.height / 2
+    }
   }
   draw() {
     c.fillStyle = "red";
@@ -27,23 +31,27 @@ class Enemy {
     this.draw();
 
     const waypoint = waypoints[this.waypointIndex];
-    const yDistance = waypoint.y - this.position.y;
-    const xDistance = waypoint.x - this.position.x;
+    const yDistance = waypoint.y - this.center.y;
+    const xDistance = waypoint.x - this.center.x;
     const angle = Math.atan2(yDistance, xDistance);
 
     this.position.x += Math.cos(angle);
     this.position.y += Math.sin(angle);
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
 console.log(this.position.x, this.position.y, waypoint.x, waypoint.y);
     if (
-      Math.round(this.position.x) === Math.round(waypoint.x) &&
-      Math.round(this.position.y) === Math.round(waypoint.y) && this.waypointIndex < waypoints.length - 1
+      Math.round(this.center.x) === Math.round(waypoint.x) &&
+      Math.round(this.center.y) === Math.round(waypoint.y) && this.waypointIndex < waypoints.length - 1
     ) {
       this.waypointIndex++;
     }
   }
 }
 const enemy = new Enemy({ position: { x: waypoints[0].x, y: waypoints[0].y } });
-const enemy2 = new Enemy({ position: {x: waypoints[0].x - 150, y: waypoints[0].y} });
+const enemy2 = new Enemy({ position: {x: waypoints[0].x - 250, y: waypoints[0].y} });
 
 
 function animate() {
