cf38cd2ea6ab2d688b95281a0c76f37b73ced599
Alex <alexflorea123@gmail.com>
2024-05-07 10:36:12 -0400

    PathingFullyDone

--- js/index.js
+++ js/index.js
@@ -50,16 +50,20 @@ console.log(this.position.x, this.position.y, waypoint.x, waypoint.y);
     }
   }
 }
-const enemy = new Enemy({ position: { x: waypoints[0].x, y: waypoints[0].y } });
-const enemy2 = new Enemy({ position: {x: waypoints[0].x - 250, y: waypoints[0].y} });
+
+const enemies = [];
+for(let i = 0; i < 10; i++){
+  const xOffset = i * 150
+  enemies.push(new Enemy({ position: { x: waypoints[0].x - xOffset, y: waypoints[0].y } }));
+}
 
 
 function animate() {
   requestAnimationFrame(animate);
 
   c.drawImage(image, 0, 0);
-  enemy.update();
-  enemy2.update();
+  enemies.forEach((enemy) => enemy.update());
+ 
 }
 
 animate();
